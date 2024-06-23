2baba189721a07d767a7b0eb4bf5d7734350cfd0
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-06-23 21:26:53 +0100

    add context

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,8 +5,11 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="container text-center mt-5">
-        <h1 className="title">Welcome to Our Landing Page</h1>
-        <p className="description">This is a brief description of what our website is about.</p>
+        <h1 className="title">Display Ad Banners</h1>
+        <p className="description">Below is a sample of display ad banners I coded using HTML,
+          CSS, JavaScript & Greensock. How I do this is by creating a template using JavaScript and
+          then I feed in copy and images from an Excel sheet to mass produce creative variants
+          for my clients.</p>
         <div className="row justify-content-center">
           {Array.from({ length: 6 }, (_, index) => (
             <div key={index} className="col-12 col-sm-6 col-md-4 d-flex justify-content-center mt-5">
